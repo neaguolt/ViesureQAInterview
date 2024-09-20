5dae460fc9e695b821a2feab858d113144ed4acc
Mihai <neaguolt@gmail.com>
2024-09-20 13:53:20 +0300

    Implement searchSelectOptionByText using Optional

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -6,15 +6,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Optional;
+
 
 public class MainPage {
     private final WebDriver driver;
     private final By searchFieldNav = By.xpath("//*[@id=\"desktop-menu\"]/form/input[1]");
     private final By searchFieldWidget = By.xpath("//*[@id=\"weather-widget\"]/div[2]/div/div/div[2]/div[1]/div/input");
     private final By dropdownOptions = By.xpath("//ul[@class='search-dropdown-menu']/li");
+    private static final Logger logger = LoggerFactory.getLogger(MainPage.class);
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
@@ -42,17 +47,21 @@ public class MainPage {
         List<WebElement> optionList =  searchDropdownByText(searchText);
 
         // Iterate over the options and find the one that matches the desired text
-        for (WebElement option : optionList) {
-            List<WebElement> spanList = option.findElements(By.tagName("span"));
-            for (WebElement span : spanList) {
-                if (span.getText().equalsIgnoreCase(optionText)) {
-                    // Click on the matching option
-                    option.click();
-                    return;
+        Optional<WebElement> elementToClick = optionList.stream()
+                .flatMap(option ->option.findElements(By.tagName("span")).stream()
+                        .filter(span -> span.getText().equalsIgnoreCase(optionText))
+                        .map(span->option))
+                        .findFirst();
+
+        elementToClick.ifPresentOrElse(
+                element -> {
+                    element.click();
+                    logger.info("Clicked on the option with text: {}", searchText);
+                },
+                () -> {
+                    // If no matching element is found, print a message
+                    throw new NoSuchElementException("No dropdown option found with text: " + optionText);
                 }
-            }
-        }
-        // If no matching option is found, return a message or handle it accordingly
-        throw new NoSuchElementException("No dropdown option found with text: " + optionText);
+        );
     }
 }
